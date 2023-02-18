0f627a489aca70079be2b46cc63db2f7fbd8a309
danielkpobari <danielkpobari@gmail.com>
2023-02-18 06:00:54 +0100

    first commit

---
+++ src/test/java/com/daniel_wizer/libraryapplication/LibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daniel_wizer.libraryapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
